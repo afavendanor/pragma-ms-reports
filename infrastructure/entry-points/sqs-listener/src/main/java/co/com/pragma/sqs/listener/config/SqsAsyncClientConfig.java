5bf459a4484443d9b8ae0f60a02be80e1f8843bd
Andres Avendaño <afavendanora@gmail.com>
2025-09-15 15:50:47 -0500

    feat: Ajuste de credenciales de sqs

--- infrastructure/entry-points/sqs-listener/src/main/java/co/com/pragma/sqs/listener/config/SqsAsyncClientConfig.java
+++ infrastructure/entry-points/sqs-listener/src/main/java/co/com/pragma/sqs/listener/config/SqsAsyncClientConfig.java
@@ -3,7 +3,7 @@ package co.com.pragma.sqs.listener.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
@@ -17,7 +17,18 @@ public class SqsAsyncClientConfig {
     public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
                 .region(Region.of(properties.region()))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(getProviderChain())
+                .build();
+    }
+
+    private AwsCredentialsProviderChain getProviderChain() {
+        return AwsCredentialsProviderChain.builder()
+                .addCredentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .addCredentialsProvider(SystemPropertyCredentialsProvider.create())
+                .addCredentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+                .addCredentialsProvider(ProfileCredentialsProvider.create())
+                .addCredentialsProvider(ContainerCredentialsProvider.builder().build())
+                .addCredentialsProvider(InstanceProfileCredentialsProvider.create())
                 .build();
     }
 
